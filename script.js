37e0eed6f8c034b4a027c6a23a2a5131f0abbbdd
JosephLee0 <josepholaniyan88@gmail.com>
2022-07-29 17:27:37 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,50 @@
+// const quest = [
+//   {
+//     question: "president",
+//     Options: ["buhari", "jonat", "nathan", "segun"],
+//     correctAnswer: "buhari",
+//     chosenAnswer: "",
+//   },
+//   {
+//     question: "president2",
+//     Options: ["buhari2", "jonat2", "nathan2", "segun2"],
+//     correctAnswer: "buhari2",
+//     chosenAnswer: "",
+//   },
+//   {
+//     question: "president3",
+//     Options: ["buhari3", "jonat3", "nathan3", "segun3"],
+//     correctAnswer: "buhari3",
+//     chosenAnswer: "",
+//   },
+//   {
+//     question: "president4",
+//     Options: ["buhari4", "jonat4", "nathan4", "segun4"],
+//     correctAnswer: "buhari4",
+//     chosenAnswer: "",
+//   },
+//   {
+//     question: "president5",
+//     Options: ["buhari5", "jonat5", "nathan5", "segun5"],
+//     correctAnswer: "buhari5",
+//     chosenAnswer: "",
+//   },
+// ];
+
+// function store() {
+//   localStorage.setItem("question", JSON.stringify(quest));
+// }
+// store();
+
+// function get() {
+//   return localStorage.getItem("question");
+// }
+
+// let data = JSON.parse(get());
+// console.log(data);
+
+// quest[0].question = "president General";
+// store();
+// data = JSON.parse(get());
+// console.log("2", data);
+// localStorage.clear();
