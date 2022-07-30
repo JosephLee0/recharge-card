a2ff229f539aad3d023e98ea0bf4fb7a4c98ad4c
JosephLee0 <josepholaniyan88@gmail.com>
2022-07-30 04:38:25 +0100

    last added

--- script.js
+++ script.js
@@ -53,6 +53,119 @@ let logoDisplay = document.querySelector(".logoD");
 let amountGenerated = document.querySelector("#amnt");
 let networkProvider = document.querySelector("#network");
 let loadPin = document.querySelector("#loading");
+let tableR = document.querySelector(".showT");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
 
-function pinGenerator() {}
-function loadCard() {}
+let data = [];
+
+function claerStorage() {
+  localStorage.clear();
+  location.reload();
+}
+
+function displayModal(html) {
+  modal.innerHTML = "";
+  modal.innerHTML = `<div class="close-modal">
+  <button onclick="closeModal()">OK</button>
+</div>`;
+  modal.classList.remove("hidden");
+  modal.innerHTML += html;
+}
+
+function closeModal() {
+  modal.classList.add("hidden");
+}
+
+function loadStorageData() {
+  let items = localStorage.getItem("cardTable");
+  if (items) {
+    data = JSON.parse(items);
+    displayTable();
+  }
+}
+loadStorageData();
+
+function storeData() {
+  localStorage.setItem("cardTable", JSON.stringify(data));
+  displayTable();
+}
+
+function pinGenerator() {
+  const time = new Date();
+  let typeCard = networkProvider.value;
+  const amount = amountGenerated.value;
+  if (!amount || amount < 100 || amount > 500) {
+    let html = `<div><h3>Amount must be between 100 and 500 </h3></div>`;
+    displayModal(html);
+    return;
+  }
+  let pin = Math.trunc(100000000000 + Math.random() * 900000000000);
+  if (typeCard == "mtn") pin = `*556*` + pin + "#";
+  if (typeCard == "glo") pin = `*124*` + pin + "#";
+  if (typeCard == "airtel") pin = `*126*` + pin + "#";
+  if (typeCard == "vodacom") pin = `*444*` + pin + "#";
+  const subElement = {
+    cardType: typeCard,
+    amount: amount,
+    pin: pin,
+    time: time,
+    status: "UNSED",
+  };
+  data.push(subElement);
+  storeData();
+}
+
+function displayTable() {
+  let k = 0;
+  tableR.innerHTML = `<tr>
+  <th>S/N</th>
+  <th>NETWORK</th>
+  <th>AMOUNT</th>
+  <th>PIN</th>
+  <th>CREATED AT</th>
+  <th>STATUS</th>
+</tr>`;
+  data.forEach((element) => {
+    let col =
+      element.status == "USED" ? "rgb(238, 127, 127)" : "rgb(193, 240, 123)";
+    tableR.innerHTML += `<tr>
+  <td>${k + 1}</td>
+  <td>${element.cardType}</td>
+  <td>${element.amount}</td>
+  <td>${element.pin}</td>
+  <td>${element.time}</td>
+  <td style="background-color:${col} ;">${element.status}</td>
+</tr>`;
+    k++;
+  });
+}
+
+function loadCard() {
+  let match;
+  let onPIn = loadPin.value;
+  if (!onPIn) {
+    let html = `<div><h3>PLEASE ENTER PIN</h3></div>`;
+    displayModal(html);
+    return;
+  }
+  match = data.findIndex((element) => {
+    return element.pin === onPIn;
+  });
+  console.log(match);
+  if (match < 0) {
+    let html = `<div><h3>INVALID PIN</h3></div>`;
+    displayModal(html);
+    return;
+  }
+  if (match >= 0 && data[match].status == "UNSED") {
+    let html = `<div><h3>CARD LOADED SUCCESFULLY</h3></div>`;
+    displayModal(html);
+    data[match].status = "USED";
+    storeData();
+  } else {
+    let html = `<div><h3>CARD HAS ALREADY BEEN USED</h3></div>`;
+    displayModal(html);
+    return;
+  }
+}
